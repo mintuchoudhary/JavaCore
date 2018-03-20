14788a9d703a6565cc5c740ae7b5b1004b01bbb2
Mintu Choudhary <mintu_choudhary01@yahoo.in>
2018-03-20 18:28:33 +0530

    Add files via upload

---
+++ src/com/singleton/SingletonDemo.java
@@ -0,0 +1,32 @@
+package com.singleton;
+class Singleton {
+	private static Singleton singletonObj = null;
+	public String s;
+
+	private Singleton() {
+		s = "value set for s in private constructor";
+	}
+
+	public static Singleton getInstance() {
+		if (singletonObj == null)
+
+			singletonObj = new Singleton();
+
+		return singletonObj;
+	}
+}
+
+public class SingletonDemo {
+	public static void main(String args[]) {
+		Singleton obj1 = Singleton.getInstance();
+		Singleton obj2 = Singleton.getInstance();
+		Singleton obj3 = Singleton.getInstance();
+
+		System.out.println(obj1 + " " + obj2 + " " + obj3);
+		System.out.println("value of s=" + obj1.s);
+		obj1.s = "new value";
+		System.out.println("value of s=" + obj2.s);
+
+		System.out.println(obj1 + " " + obj2);
+	}
+}
