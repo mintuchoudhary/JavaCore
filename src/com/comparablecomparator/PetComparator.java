b507f3a1b73448b3b0de6172cb6b93c800489887
Mintu Choudhary <mintu_choudhary01@yahoo.in>
2018-03-23 19:32:03 +0530

    multiple parameter compare

--- src/com/comparablecomparator/PetComparator.java
+++ src/com/comparablecomparator/PetComparator.java
@@ -13,6 +13,27 @@ public class PetComparator implements Comparator {
 
 		return pet1.getPetType().compareTo(pet2.getPetType());
 	}
+	
+/*
+//For multiple comparision i.e will compare other parameter if first parameter is same
+public int compare(Object o1, Object o2) {
+
+    String x1 = ((Pet) o1).getPetType();
+    String x2 = ((Pet) o2).getPetType();
+    int sComp = x1.compareTo(x2);
+
+    if (sComp != 0) {
+       return sComp;
+    } else {
+       Integer pid1 = ((Pet) o1).getPetId();
+       Integer pid2 = ((Pet) o2).getPetId();
+       System.out.println("---"+((Pet) o1).getPetType());
+       System.out.println("----"+((Pet) o2).getPetType());
+       return pid1.compareTo(pid2);
+    }
+}
+
+*/
 
 	public static void main(String args[]) {
 		List petlist = new ArrayList<Pet>();
