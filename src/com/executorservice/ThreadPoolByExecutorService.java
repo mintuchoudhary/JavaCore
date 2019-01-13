9715a1eeb3f4f30758cdc0a7f7402420aa639a40
Mintu Choudhary <mintu_choudhary01@yahoo.in>
2019-01-13 20:53:16 +0530

    Update ThreadPoolByExecutorService.java

--- src/com/executorservice/ThreadPoolByExecutorService.java
+++ src/com/executorservice/ThreadPoolByExecutorService.java
@@ -44,12 +44,22 @@ class WorkerThread implements Runnable {
  *  it will start working on 5 jobs and other jobs will be in wait state, 
  *  as soon as one of the job is finished, 
  *  another job from the wait queue will be picked up by worker thread and get's executed.
+ 
+ newFixedThreadPool      - creates a pool with fixed number of thread given
+ newSingleThreadExecutor - Single thread will be responsible for executing all 10 jobs.
+ newCachedThreadPool     - creates a thread pool that creates new thread as needed, but will reuse 
+                           previous constructed thread when they are available
+newScheduledThreadPool   - when you want to execute a task after a period of time or periodically			   
  * */
 
 public class ThreadPoolByExecutorService {
 	public static void main(String[] args) {
 		ExecutorService executor = Executors.newFixedThreadPool(5);
+		//ExecutorService executor = Executors.newSingleThreadExecutor(); //create a single thread pool
+		//ExecutorService executor = Executors.newCachedThreadPool();
+		//ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 		//ThreadByCallable thread = new ThreadByCallable();
+		
 		for (int i = 0; i < 10; i++) {
 			Runnable worker = new WorkerThread("" + i);
 			executor.execute(worker);
