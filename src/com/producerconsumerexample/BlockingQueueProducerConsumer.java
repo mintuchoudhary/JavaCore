724eded5a69a12d6e4d7772a544fab83dd6ffd93
Mintu Choudhary <mintu_choudhary01@yahoo.in>
2020-09-07 02:01:29 +0530

    Update BlockingQueueProducerConsumer.java

--- src/com/producerconsumerexample/BlockingQueueProducerConsumer.java
+++ src/com/producerconsumerexample/BlockingQueueProducerConsumer.java
@@ -3,6 +3,15 @@ package com.producerconsumerexample;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
+/*
+Java BlockingQueue interface is part of java collections framework and it’s primarily used for implementing 
+producer consumer problem. 
+We don’t need to worry about waiting for the
+ space to be available for producer or 
+object to be available for consumer in
+ BlockingQueue because it’s handled by 
+implementation classes of BlockingQueue.
+*/
 class Producer implements Runnable {
 	BlockingQueue<Integer> queue;
 
@@ -50,7 +59,7 @@ class Consumer implements Runnable {
 
 public class BlockingQueueProducerConsumer {
 	public static void main(String[] args) {
-
+         //creating blocking queue of size 3
 		BlockingQueue<Integer> queue = new ArrayBlockingQueue<Integer>(3);
 		Thread producerThread = new Thread(new Producer(queue));
 		Thread consumerThread = new Thread(new Consumer(queue));
