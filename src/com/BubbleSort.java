911d93f00c9096d265d6ac40fa02423567e9f7d9
Mintu Choudhary <mintuchoudhary01@gmail.com>
2020-09-15 01:34:36 +0530

    Update BubbleSort.java

--- src/com/BubbleSort.java
+++ src/com/BubbleSort.java
@@ -1,7 +1,19 @@
 package com.java8;
 
 // Java program for implementation of Bubble Sort
+/*
+First Pass:
+( 5 1 4 2 8 ) –> ( 1 5 4 2 8 ), Here, algorithm compares the first two elements, and swaps since 5 > 1.
+( 1 5 4 2 8 ) –>  ( 1 4 5 2 8 ), Swap since 5 > 4
+( 1 4 5 2 8 ) –>  ( 1 4 2 5 8 ), Swap since 5 > 2
+( 1 4 2 5 8 ) –> ( 1 4 2 5 8 ), Now, since these elements are already in order (8 > 5), algorithm does not swap them.
 
+Second Pass:
+( 1 4 2 5 8 ) –> ( 1 4 2 5 8 )
+( 1 4 2 5 8 ) –> ( 1 2 4 5 8 ), Swap since 4 > 2
+( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
+( 1 2 4 5 8 ) –>  ( 1 2 4 5 8 )
+    */
 /**
  * Sorted array
  * 11 12 22 25 34 64 90
