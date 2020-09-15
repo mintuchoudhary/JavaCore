b4e59f0c75f12710d19bc8121856d47e62748936
Mintu Choudhary <mintuchoudhary01@gmail.com>
2020-09-16 00:20:52 +0530

    SOLID principle

---
+++ src/com/SOLID.java
@@ -0,0 +1,31 @@
+public class SOLID {
+    /**
+     S for Single Responsibility Principle
+     O for Open/Closed Principle
+     L for Liskov Substitution Principle
+     I for Interface Segregation Principle
+     D for Dependency Inversion Principle
+
+Single Responsibility Principle:
+
+     It states that in a well-designed application, each class (microservice, code module) should have only one single responsibility.
+
+Open/Closed Principle:
+
+     The Open/Closed Principle states that classes, modules, microservices, and other code units should be open for extension but closed for modification.
+
+Liskov Substitution Principle:
+
+     it states that an object of a superclass should be replaceable by objects of its subclasses without causing issues in the application. child class acts as substitute of the parent class
+
+Interface Segregation Principle:
+
+     Clients should not be forced to depend on methods they don’t use.” By clients, he means classes that implement interfaces
+     The violation of this principle forces user to write dummy methods that do nothing.
+
+Dependency Inversion Principle:
+     The goal of the Dependency Inversion Principle is to avoid tightly coupled code,
+     as it easily breaks the application
+
+     */
+}
