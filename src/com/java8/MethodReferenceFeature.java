e8f76582b3541cd4100f1c0126561390eb3322a3
Mintu Choudhary <mintuchoudhary01@gmail.com>
2020-09-15 10:40:42 +0530

    Update MethodReferenceFeature.java

--- src/com/java8/MethodReferenceFeature.java
+++ src/com/java8/MethodReferenceFeature.java
@@ -5,7 +5,12 @@ import java.util.List;
 
 /*
  * Method references help to point to methods by their names. A method reference is described using "::" symbol.
- * */
+ * 
+Reference to a static method.
+Reference to an instance method.
+Reference to a constructor.
+
+*/
 public class MethodReferenceFeature {
 	public static void main(String[] args) {
 
@@ -13,12 +18,49 @@ public class MethodReferenceFeature {
 
 		names.add("Mahesh");
 		names.add("Suresh");
-		names.add("Ramesh");
-		names.add("Naresh");
-		names.add("Kalpesh");
-
 		//names.forEach(i->System.out.println(i));
 		// WITH METHOD REFERENCE
 		names.forEach(System.out:: println);
-	}
+	
+           // Referring static method  
+           Sayable sayable = MethodReference::saySomething;  
+           // Calling interface method  
+           sayable.say();  
+ 
+         Thread t2=new Thread(MethodReference::saySomething);  
+          t2.start();  
+
+           
+         MethodReference methodReference = new MethodReference(); // Creating object  
+          // Referring non-static method using reference  
+            Sayable sayable2 = methodReference::printSomething;  
+          // Calling interface method  
+            sayable2.say();  
+
+             Messageable hello = Message::new;  
+            hello.getMessage("Hello");  
+      }
 }
+
+//static method reference
+interface Sayable{  
+    void say();  
+}  
+class MethodReference {  
+    public static void saySomething(){  
+        System.out.println("Hello, this is static method.");  
+    }  
+   public void printSomething() {
+      System.out.println("in print method");
+   } 
+}  
+
+interface Messageable{  
+    Message getMessage(String msg);  
+}  
+class Message{  
+    Message(String msg){  
+        System.out.print(msg);  
+    }  
+}
+
