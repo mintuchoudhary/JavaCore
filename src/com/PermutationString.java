65b629062af9ac16a61c1100221f4d9b899e7c33
Mintu Choudhary <mintuchoudhary01@gmail.com>
2020-10-16 11:22:56 +0530

    Add files via upload

---
+++ src/com/PermutationString.java
@@ -0,0 +1,29 @@
+package com.utilities;
+
+public class PermutationString {
+    public static void permutation(String str) {
+        permutation("", str);
+    }
+
+    private static void permutation(String prefix, String str) {
+        int n = str.length();
+        if (n == 0) System.out.println(prefix);
+        else {
+            for (int i = 0; i < n; i++)
+                permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
+        }
+    }
+
+    public static void main(String[] args) {
+        permutation("ABC");
+    }
+}
+
+/**
+ ABC
+ ACB
+ BAC
+ BCA
+ CAB
+ CBA
+ */
