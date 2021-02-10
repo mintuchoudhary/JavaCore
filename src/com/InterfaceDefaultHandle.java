75dfeb39cb7b17c0b3899f4b30545cea44f88479
Mintu Choudhary <mintu_choudhary01@yahoo.in>
2021-02-10 23:20:15 +0530

    inteface default handle

---
+++ src/com/InterfaceDefaultHandle.java
@@ -0,0 +1,36 @@
+package com;
+
+/**
+ * If you dont override the display method in main the you will compilation error at implements I1,I2 line
+ * hello
+ * test class 2
+ */
+interface I1 {
+    default void display(){
+        System.out.println( "i1" );
+    }
+}
+
+interface I2 {
+    default void display(){
+        System.out.println( "i2" );
+    }
+
+
+}
+
+public class InterfaceDefaultHandle implements I1,I2 {
+
+    @Override
+    public void display() {
+        System.out.println( "test class 2" );
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println( "hello") ;
+       new InterfaceDefaultHandle().display();
+    }
+}
+
+
